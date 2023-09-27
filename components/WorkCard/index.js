b5b3fd84de0308f52efb01b6ac28daa0770930c8
jalapenojames <61204374+jalapenojames@users.noreply.github.com>
2023-09-27 14:47:22 -0400

    updated projects

--- components/WorkCard/index.js
+++ components/WorkCard/index.js
@@ -6,11 +6,23 @@ const WorkCard = ({ img, name, description, onClick }) => {
   const Player = props => {
     let videourl = props.videourl.replace('.gifv', '.mp4');
     return <video src={ videourl } className={props.className} />;
- };
+  };
+
+  const SplineScene = React.memo(function SplineScene() {
+    return (
+      <Suspense fallback={<div>Loading...</div>}>
+        <Spline 
+          style={{ height: "600px" }}
+          scene="https://prod.spline.design/DQCXjsdx1QqDfvPL/scene.splinecode" 
+        />     
+      </Suspense>
+    );
+  });
+
 
   if(name === "Well Ping @SSNL")
     return (
-  <div
+      <div
         className="overflow-hidden rounded-lg p-2 laptop:p-4 first:ml-0 link"
         onClick={onClick}
       >
@@ -18,13 +30,13 @@ const WorkCard = ({ img, name, description, onClick }) => {
           className="relative rounded-lg overflow-hidden transition-all ease-out duration-300 h-48 mob:h-auto bg-indigo-500"
           style={{ height: "600px" }}
         >
-          <Suspense fallback={<div>Loading...</div>}>
+          <SplineScene />
+          {/* <Suspense fallback={<div>Loading...</div>}>
              <Spline 
-                className="relative rounded-lg overflow-hidden transition-all ease-out duration-300 h-48 mob:h-auto bg-indigo-500"
                 style={{ height: "600px" }}
                 scene="https://prod.spline.design/DQCXjsdx1QqDfvPL/scene.splinecode" 
              />
-          </Suspense> 
+          </Suspense>  */}
           {/* <img
             alt={name}
             className="h-full w-full object-cover hover:scale-110 transition-all ease-out duration-300"
@@ -39,7 +51,68 @@ const WorkCard = ({ img, name, description, onClick }) => {
         </h2>
       </div>
   )
-    
+  
+  // if(name === "MakolaHub")
+  //   return (
+  //     <div
+  //       className="overflow-hidden rounded-lg p-2 laptop:p-4 first:ml-0 link"
+  //       onClick={onClick}
+  //     >
+  //       <div
+  //         className="relative rounded-lg overflow-hidden transition-all ease-out duration-300 h-48 mob:h-auto bg-indigo-500"
+  //         style={{ height: "600px" }}
+  //       >
+  //         <Suspense fallback={<div>Loading...</div>}>
+  //           <Spline 
+  //               style={{ height: "600px" }}
+  //               scene="https://prod.spline.design/C4f3A0Susxo2v0u7/scene.splinecode" 
+  //           />
+  //         </Suspense> 
+  //         {/* <img
+  //           alt={name}
+  //           className="h-full w-full object-cover hover:scale-110 transition-all ease-out duration-300"
+  //           src={"https://i.imgur.com/aaMtORa.gifv"}
+  //         /> */}
+  //       </div>
+  //       <h1 className="mt-5 text-3xl font-medium">
+  //         {name ? name : "Project Name"}
+  //       </h1>
+  //       <h2 className="text-xl opacity-50">
+  //         {description ? description : "Description"}
+  //       </h2>
+  //     </div>
+  //   )
+  
+  if(name === "MyFavor App")
+    return (
+      <div
+        className="overflow-hidden rounded-lg p-2 laptop:p-4 first:ml-0 link"
+        onClick={onClick}
+      >
+        <div
+          className="relative rounded-lg overflow-hidden transition-all ease-out duration-300 h-48 mob:h-auto bg-indigo-500"
+          style={{ height: "600px" }}
+        >
+          <Suspense fallback={<div>Loading...</div>}>
+            <Spline 
+                style={{ height: "600px" }}
+                scene="https://prod.spline.design/9ih9OrAmPejBLOUu/scene.splinecode" 
+            />
+          </Suspense> 
+          {/* <img
+            alt={name}
+            className="h-full w-full object-cover hover:scale-110 transition-all ease-out duration-300"
+            src={"https://i.imgur.com/aaMtORa.gifv"}
+          /> */}
+        </div>
+        <h1 className="mt-5 text-3xl font-medium">
+          {name ? name : "Project Name"}
+        </h1>
+        <h2 className="text-xl opacity-50">
+          {description ? description : "Description"}
+        </h2>
+      </div>
+    )
   return (
     <>
     
